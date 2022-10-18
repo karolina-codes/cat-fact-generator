1f8246bb0ceb52c9e78326f50b90ab05646d5d32
Karolina Joksaite <k.joksaite00@gmail.com>
2022-10-18 16:00:59 +0100

    changed func to return 1 image

--- src/services/imageApi.ts
+++ src/services/imageApi.ts
@@ -1,23 +1,9 @@
 import axios from 'axios';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
+const URL = `https://api.thecatapi.com/v1/images/search?limit=1&mime_types=jpg,png`;
 
-const URL = `https://api.thecatapi.com/v1/images/search?limit=52&mime_types=jpg,png&api_key=${process.env.API_KEY}`;
-
-interface apiObject {
-  id: string;
-  url: string;
-  width: number;
-  height: number;
-  breeds: string[];
-}
-
-export const getImages = () => {
-  axios(URL)
-    .then((response): apiObject[] => response.data)
-    .then((data: apiObject[]): string[] => {
-      const images = data.map((object: apiObject) => object.url);
-      return images;
-    });
+export const getImage = async (): Promise<string> => {
+  const response = await axios(URL);
+  const image = response.data[0].url;
+  return image;
 };
